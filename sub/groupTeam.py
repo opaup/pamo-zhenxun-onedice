e212088aaf9299327637b426f0438033fdc29e27
opaup <opaup5259@gmail.com>
2023-10-11 09:02:37 +0800

    代码与结构调整

---
+++ sub/groupTeam.py
@@ -0,0 +1,9 @@
+# kp|dm 设自己为kp|dm
+# ob 进入ob队列
+# team 查看团队列表
+# team clear/clr/cls 清空队伍
+# team add @at|uid 添加到队伍
+# team rm/del @at|uid|id 从队伍删除
+# team call 呼叫队伍全体成员
+# team @at|uid propvalue 调整成员卡属性
+# team lock 一键全体卡上锁
