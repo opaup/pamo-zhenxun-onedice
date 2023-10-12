4ae34e7f24343ad63dc5b1bae7383bd5d3747f7f
opaup <opaup5259@gmail.com>
2023-10-12 14:59:57 +0800

    说明更新

--- sub/groupTeam.py
+++
@@ -1,9 +0,0 @@
-# kp|dm 设自己为kp|dm -- 秘密团
-# ob 进入ob队列 -- 秘密团
-# team 查看团队列表
-# team clear/clr/cls 清空队伍
-# team add @at|uid 添加到队伍
-# team rm/del @at|uid|id 从队伍删除
-# team call 一键呼叫队伍全体成员
-# team @at|uid propvalue 调整成员卡属性
-# team lock 队伍内一键全体角色卡上锁
